6caf09eb94c8e53dda58e5bc99ab71501b78cf72
Laura Noges <lnoges@fredhutch.org>
2018-02-28 08:27:58 -0800

    formatting changes to brief

--- laura-output-processing-brief.py
+++ laura-output-processing-brief.py
@@ -26,20 +26,20 @@ best_partition = cp.partitions[cp.i_best]
 # sorted_clusters = sorted(best_partition, key=len, reverse=True)  # sort by size
 
 # clonal family attributes to print
-def print_sizeindex(cluster):
-    cluster_index = sorted_clusters.index(cluster)
-    print 'cluster index =', cluster_index
-
+print 'index    genes                                   size  n muts  SHM   rep frac  CDR3                           SFS'
 def print_stuff(line):
-    print line['v_gene'],
-    print line['d_gene'],
-    print line['j_gene']
-    print len(line['unique_ids']), 'sequences in cluster'
-    print float(len(cluster)) / n_total, 'fraction of total repertoire'
+    cluster_index = sorted_clusters.index(cluster)
     cdr3_seq = line['naive_seq'][(line['codon_positions']['v']):((line['codon_positions']['j'])+3)] #get nt sequence of CDR3 from first base of cysteine through last base of tryptophan
-    print Seq(cdr3_seq).translate()
-    print utils.fay_wu_h(line, debug=False), 'SFS (Fay Wu H) score' # site frequency spectrum
-    print
+    print '%4s  %13s %16s %10s %5d %5d %7.3f %8.4f   %-30s %4.2f' % (cluster_index, line['v_gene'], line['d_gene'], line['j_gene'], len(line['unique_ids']), numpy.mean(line['n_mutations']), numpy.mean(line['mut_freqs']), float(len(cluster)) / n_total, Seq(cdr3_seq).translate(), utils.fay_wu_h(line, debug=False))
+#    print line['v_gene'],
+#    print line['d_gene'],
+#    print line['j_gene']
+#    print len(line['unique_ids']), 'sequences in cluster'
+#    print numpy.mean(line['n_mutations']), 'mean number of mutations'
+#    print numpy.mean(line['mut_freqs']), 'mean SHM'
+#    print float(len(cluster)) / n_total, 'fraction of total repertoire'
+#    print Seq(cdr3_seq).translate()
+#    print utils.fay_wu_h(line, debug=False), 'SFS (Fay Wu H) score' # site frequency spectrum
 
 # formatting necessity
 def getkey(uid_list):
@@ -70,19 +70,16 @@ sfs_clusters = sorted(biggest_clusters, key=lambda q: utils.fay_wu_h(annotations
 # cluster size: print x biggest clusters
 print '\x1b[1;32;40m' + '  printing the largest clusters' + '\x1b[0m'
 for cluster in sorted_clusters[:5]:
-    print_sizeindex(cluster)
     print_stuff(annotations[cluster])
 
 # high mean %SHM: print most mutated clusters from 100 biggest clusters
 print '\x1b[1;32;40m' + '  printing the most mutated clusters (within 100 biggest)' + '\x1b[0m'
 for cluster in shm_clusters[:5]:
-    print_sizeindex(cluster)
     print_stuff(annotations[cluster])
 
 # Highest SFS Fay Wu H scores
 print '\x1b[1;32;40m' + '  printing clusters with high SFS (within 100 biggest)' + '\x1b[0m'
 for cluster in sfs_clusters[:5]:
-    print_sizeindex(cluster)
     print_stuff(annotations[cluster])
 
 # published bnAb VH gene usage (Yu and Guan, Frontiers in Immunology, 2014)
@@ -103,7 +100,6 @@ interesting_clusters = [cluster for cluster in sorted_clusters if boolfunc(clust
 print '\x1b[1;32;40m' + '  found %d cd4bs clusters, printing the 5 largest hits' % len(interesting_clusters)
 sorted_interesting = sorted(interesting_clusters, key=lambda q: len(annotations[q]['unique_ids']), reverse=True)
 for cluster in sorted_interesting[0:5]: # five biggest cd4bs clusters
-    print_sizeindex(cluster)
     print_stuff(annotations[cluster])
 
 
@@ -119,7 +115,6 @@ interesting_clusters = [cluster for cluster in sorted_clusters if boolfunc(clust
 print '\x1b[1;32;40m' + '  found %d glycan clusters, printing the 5 largest hits' % len(interesting_clusters)
 sorted_interesting = sorted(interesting_clusters, key=lambda q: len(annotations[q]['unique_ids']), reverse=True)
 for cluster in sorted_interesting[0:5]: # five biggest cd4bs clusters
-    print_sizeindex(cluster)
     print_stuff(annotations[cluster])
 
 
@@ -135,7 +130,6 @@ interesting_clusters = [cluster for cluster in sorted_clusters if boolfunc(clust
 print '\x1b[1;32;40m' + '  found %d bridging region clusters, printing the 5 largest hits' % len(interesting_clusters)
 sorted_interesting = sorted(interesting_clusters, key=lambda q: len(annotations[q]['unique_ids']), reverse=True)
 for cluster in sorted_interesting[0:5]: # five biggest cd4bs clusters
-    print_sizeindex(cluster)
     print_stuff(annotations[cluster])
 
 
@@ -151,7 +145,6 @@ interesting_clusters = [cluster for cluster in sorted_clusters if boolfunc(clust
 print '\x1b[1;32;40m' + '  found %d mper region clusters, printing the 5 largest hits' % len(interesting_clusters)
 sorted_interesting = sorted(interesting_clusters, key=lambda q: len(annotations[q]['unique_ids']), reverse=True)
 for cluster in sorted_interesting[0:5]: # five biggest cd4bs clusters
-    print_sizeindex(cluster)
     print_stuff(annotations[cluster])
 
 
